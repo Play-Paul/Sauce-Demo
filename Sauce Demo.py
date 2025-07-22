3318b103e43aaca078278f519a90f9f2508295d1
Play-Paul <info.playpro@gmail.com>
2025-07-22 03:33:40 +0100

    Sauce Demo

--- Sauce Demo.py
+++ Sauce Demo.py
@@ -20,5 +20,14 @@ driver.find_element(By.XPATH, "(//input[@id='login-button'])[1]").click()
 driver.find_element(By.XPATH, "//div[@class='inventory_list']//div[1]//div[3]//button[1]").click()
 driver.find_element(By.CSS_SELECTOR, "body > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > button:nth-child(2)").click()
 driver.find_element(By.XPATH, "//div[6]//div[3]//button[1]").click()
-driver.find_element(By.CLASS_NAME, "btn_primary btn_inventory").click()
-driver.find_element(By.XPATH, "(//*[name()='path'][@fill='currentColor'])[1]").click()
+driver.find_element(By.XPATH, "/html/body/div/div[2]/div[2]/div/div[2]/div/div[3]/div[3]/button").click()
+
+#Go to CART
+driver.find_element(By.CSS_SELECTOR, "a[class='shopping_cart_link fa-layers fa-fw'] svg path").click()
+time.sleep(3)
+
+#CHECKOUT and LOGOUT
+driver.find_element(By.XPATH, "/html/body/div/div[2]/div[3]/div/div[2]/a[2]")
+driver.find_element(By.XPATH, "//*[@id='menu_button_container']/div/div[3]/div/button").click()
+time.sleep(2)
+driver.find_element(By.XPATH, "//*[@id='logout_sidebar_link']").click()
